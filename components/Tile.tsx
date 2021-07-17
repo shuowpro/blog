9173ac810f93a63ce76efbd5adb1d3e49292d49c
leuction <leuction@gmail.com>
2021-07-17 01:12:46 -0700

    change the col to be 2

--- components/Tile.tsx
+++ components/Tile.tsx
@@ -80,7 +80,7 @@ export const Tiles: FC<{
 }> = ({ posts, className }) => {
   return (
     <div className="container my-8">
-      <div className="grid grid-cols-1 sm:grid-cols-3 gap-4 my-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 my-4">
         {posts.map((post) => (
           <Tile key={post.id} post={post} />
         ))}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,4 @@
 import { AppProps } from 'next/app'
-import Head from 'next/head'
-import Layout from '../components/Layout'
 
 import '../styles/globals.css'
 import '../styles/prism.css'
