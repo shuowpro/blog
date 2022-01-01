fa6d63c99b1ac735c04ed46f270181df88399da6
leuction <leuction@gmail.com>
2021-12-31 18:37:59 -0800

    Update the style

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,7 +4,7 @@ import Gravatar from 'react-gravatar';
 
 export default function Header() {
   return (
-    <div className="sticky top-0 z-40 w-full backdrop-blur flex-none transition-colors duration-500 lg:z-50 lg:border-b lg:border-stone-900/10 bg-white/50 shadow-lg">
+    <div className="sticky top-0 z-40 w-full backdrop-blur flex-none transition-colors duration-500 lg:z-50 lg:border-b lg:border-stone-900/10 bg-white/10 shadow-lg">
       <div className="max-w-8xl mx-auto">
         <div className="py-4 border-b border-stone-900/10 lg:px-8 lg:border-0 mx-4">
           <div className="relative flex items-center">
