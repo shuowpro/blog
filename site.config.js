18b083e55246b7547a8bfbe0bedb79c1d5357611
suwako <i@suwako.dev>
2022-03-16 22:30:16 -0700

    Update the title

--- site.config.js
+++ site.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  title: 'keiki.dev',
+  title: 'suwako.dev',
   description: '认真你就输了',
-  email: 'leuction@gmail.com',
+  email: 'i@suwako.dev',
   cardImage:
     'https://og-image.now.sh/**Prism**%20with%20Next.js.png?theme=dark&md=1&fontSize=100px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-white-logo.svg'
 };
