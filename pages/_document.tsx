7e96f74e9680d925d30b0f3c083d5a765887e5b9
suwako <i@suwako.dev>
2022-07-31 21:26:14 -0700

    Update lang

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,7 @@ import { Head, Html, Main, NextScript } from 'next/document';
 
 export default function Document() {
 	return (
-		<Html lang="en">
+		<Html lang="zh-Hans">
 			<Head>
 				<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
 				<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
