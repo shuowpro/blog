ffee6bd7503f93ae04389d32739f41f92c66aca0
shuowpro <shuowpro@gmail.com>
2025-05-11 12:48:10 -0700

    fix build

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,17 +2,17 @@ import rss from '@astrojs/rss'
 import { getCollection } from 'astro:content'
 import { getPostTitle, getPublishDate } from '../utils'
 
-export async function GET(context) {
-  const blog = await getCollection('blog')
-  return rss({
-    title: '虹色的世界',
-    description: '虹色的世界',
-    site: context.site,
-    items: blog.map((post) => ({
-      title: getPostTitle(post),
-      pubDate: getPublishDate(post),
-      link: `/posts/${getPostTitle(post)}`,
-      content: post.rendered?.html,
-    })),
-  })
-}
+// export async function GET(context) {
+//   const blog = await getCollection('blog')
+//   return rss({
+//     title: '虹色的世界',
+//     description: '虹色的世界',
+//     site: context.site,
+//     items: blog.map((post) => ({
+//       title: getPostTitle(post),
+//       pubDate: getPublishDate(post),
+//       link: `/posts/${getPostTitle(post)}`,
+//       content: post.rendered?.html,
+//     })),
+//   })
+// }
