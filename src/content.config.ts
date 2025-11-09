d2b234fe27628bf0d795ecdf2390a4b0c85d485a
shuowpro <shuowpro@gmail.com>
2025-11-09 11:04:34 -0800

    add content

--- src/content.config.ts
+++ src/content.config.ts
@@ -6,19 +6,20 @@ const posts = defineCollection({
     pattern: '**/*.md',
     base: 'src/contents/posts',
   }),
-  schema: z.object({
-    title: z.string(),
-    published: z.date(),
-    draft: z.boolean().optional(),
-    description: z.string().optional(),
-    cover: z.string().optional(),
-    tags: z.array(z.string()).optional(),
-    category: z.string().optional(),
-    author: z.string().optional(),
-    sourceLink: z.string().optional(),
-    licenseName: z.string().optional(),
-    licenseUrl: z.string().optional(),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      published: z.date(),
+      draft: z.boolean().optional(),
+      description: z.string().optional(),
+      preview: image().optional(),
+      tags: z.array(z.string()).optional(),
+      category: z.string().optional(),
+      author: z.string().optional(),
+      sourceLink: z.string().optional(),
+      licenseName: z.string().optional(),
+      licenseUrl: z.string().optional(),
+    }),
 })
 
 const specs = defineCollection({
